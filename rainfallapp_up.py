989c9ffca55023731c35ca1acb64f2bb79e12bec
SaipriyaSR <ce21resch11004@iith.ac.in>
2025-11-13 19:15:30 +0530

    Updated rainfall_app.py with new features

--- rainfallapp_up.py
+++ rainfallapp_up.py
@@ -101,7 +101,7 @@ You can:
                 
 """)
 
-uploaded_file = st.sidebar.file_uploader("📂 Upload hourly rainfall CSV file )
+uploaded_file = st.sidebar.file_uploader("📂 Upload hourly rainfall CSV file ")
 
 # =========================
 # MAIN BODY
